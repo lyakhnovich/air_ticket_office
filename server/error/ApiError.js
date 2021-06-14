1c2861f5b92495fe1a87d371f39e67e8b4778d82
lyakhnovich <lyakhnovich.german@gmail.com>
2021-06-14 22:00:15 +0300

    ApiError

--- server/error/ApiError.js
+++ server/error/ApiError.js
@@ -4,4 +4,16 @@ class ApiError extends Error{
     this.status = status;
     this.message = message;
   }
+
+  static badRequest(message) {
+    return ApiError(404, message)
+  }
+
+  static internal(message) {
+    return ApiError(500, message)
+  }
+
+  static forbidden(message) {
+    return ApiError(403, message)
+  }
 }
