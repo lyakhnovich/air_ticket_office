df2914467c61e6e45df40b2c61ac926e7757a7b9
Lyakhnovich German <lyakhnovich.german@gmail.com>
2021-06-18 17:57:21 +0300

    calendar

--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+import { addDays } from 'date-fns';
+import { useState } from 'react';
+import Container from "react-bootstrap/Container";
+import DateRange from "react-date-range/dist/components/DateRange";
+
+
+
+
 const Booking = () => {
+
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: 'selection'
+    }
+  ]);
+
   return (
-    <div>
-      BOOKING
-    </div>
+    <Container>
+
+      <input/>
+
+      <DateRange
+        showDateDisplay={false}
+        editableDateInputs={true}
+        onChange={item => setState([item.selection])}
+        moveRangeOnFirstSelection={false}
+        ranges={state}
+      />
+    </Container>
   );
 };
 
 export default Booking;
+
