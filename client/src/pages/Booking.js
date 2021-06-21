c1ffb3f14a2668126a7c369777977d4d59b7bd84
Lyakhnovich German <lyakhnovich.german@gmail.com>
2021-06-21 17:32:12 +0300

    client

--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -1,42 +1,53 @@
 import React from 'react';
 
+import {DatesRangeInput} from 'semantic-ui-calendar-react';
 
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-
-import { addDays } from 'date-fns';
-import { useState } from 'react';
-import Container from "react-bootstrap/Container";
-import DateRange from "react-date-range/dist/components/DateRange";
+import Form from "semantic-ui-react/dist/commonjs/collections/Form";
+import {Container} from "react-bootstrap";
+import Icon from "semantic-ui-react/dist/commonjs/elements/Icon";
 
 
 
 
-const Booking = () => {
 
-  const [state, setState] = useState([
-    {
-      startDate: new Date(),
-      endDate: addDays(new Date(), 7),
-      key: 'selection'
+    class Booking extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        datesRange: ''
+      };
     }
-  ]);
 
-  return (
-    <Container>
+    handleChange = (event, {name, value}) => {
+      if (this.state.hasOwnProperty(name)) {
+        this.setState({ [name]: value });
+      }
+    }
 
-      <input/>
+    render() {
+      return (
+        <Container
+          className="d-flex justify-content-center align-items-center"
+          style={{height: window.innerHeight - 400}}
+        >
+          <input/>
+          <input/>
+          <Form>
 
-      <DateRange
-        showDateDisplay={false}
-        editableDateInputs={true}
-        onChange={item => setState([item.selection])}
-        moveRangeOnFirstSelection={false}
-        ranges={state}
-      />
-    </Container>
-  );
-};
+            <DatesRangeInput
+              name="datesRange"
+              placeholder="Когда - Обратно"
+              value={this.state.datesRange}
+              iconPosition="left"
+              onChange={this.handleChange}
+            />
+          </Form>
+          <input/>
+        </Container>
+      );
+    }
+  }
 
 export default Booking;
 
